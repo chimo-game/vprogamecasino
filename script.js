24a956aab666d60ce56b54cf834178e099606d15
pao-tola <73347353+pao-tola@users.noreply.github.com>
2022-03-13 18:13:59 +0700

    first commit

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
 
 class parallaxTiltEffect {
 
-  constructor({element, tiltEffect}) {
+  constructor({ element, tiltEffect }) {
 
     this.element = element;
     this.container = this.element.querySelector(".container");
@@ -32,26 +32,26 @@ class parallaxTiltEffect {
   }
 
   handleMouseMove(event) {
-    const {offsetX, offsetY} = event;
+    const { offsetX, offsetY } = event;
 
     let X;
     let Y;
 
     if (this.tiltEffect === "reverse") {
-      X = ((offsetX - (this.w/2)) / 3) / 3;
-      Y = (-(offsetY - (this.h/2)) / 3) / 3;
+      X = ((offsetX - (this.w / 2)) / 3) / 3;
+      Y = (-(offsetY - (this.h / 2)) / 3) / 3;
     }
 
     else if (this.tiltEffect === "normal") {
-      X = (-(offsetX - (this.w/2)) / 3) / 3;
-      Y = ((offsetY - (this.h/2)) / 3) / 3;
+      X = (-(offsetX - (this.w / 2)) / 3) / 3;
+      Y = ((offsetY - (this.h / 2)) / 3) / 3;
     }
 
     this.setProperty('--rY', X.toFixed(2));
     this.setProperty('--rX', Y.toFixed(2));
 
-    this.setProperty('--bY', (80 - (X/4).toFixed(2)) + '%');
-    this.setProperty('--bX', (50 - (Y/4).toFixed(2)) + '%');
+    this.setProperty('--bY', (80 - (X / 4).toFixed(2)) + '%');
+    this.setProperty('--bX', (50 - (Y / 4).toFixed(2)) + '%');
   }
 
   handleMouseEnter() {
@@ -100,3 +100,41 @@ const wrap3 = new parallaxTiltEffect({
   element: $('.wrap--3'),
   tiltEffect: 'reverse'
 });
+
+
+
+gsap.set(".ball", { xPercent: -50, yPercent: -50 });
+gsap.set(".follow-ball", { xPercent: -50, yPercent: -50 });
+
+const ball = document.querySelector(".ball");
+const follow = document.querySelector(".follow-ball");
+const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+const posF = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+const mouse = { x: pos.x, y: pos.y };
+const speed = 1;
+const speedF = 0.35;
+
+const xSet = gsap.quickSetter(ball, "x", "px");
+const ySet = gsap.quickSetter(ball, "y", "px");
+const xSetFollow = gsap.quickSetter(follow, "x", "px");
+const ySetFollow = gsap.quickSetter(follow, "y", "px");
+
+window.addEventListener("mousemove", e => {
+  mouse.x = e.x;
+  mouse.y = e.y;
+});
+
+gsap.ticker.add(() => {
+  const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+  const dtF = 1.0 - Math.pow(1.0 - speedF, gsap.ticker.deltaRatio());
+  const xSetPos = pos.x += (mouse.x - pos.x) * dt;
+  const ySetPos = pos.y += (mouse.y - pos.y) * dt;
+  const xSetFollowPos = posF.x += (mouse.x - posF.x) * dtF;
+  const ySetFollowPos = posF.y += (mouse.y - posF.y) * dtF;
+
+  xSet(xSetPos);
+  ySet(ySetPos);
+  xSetFollow(xSetFollowPos);
+  ySetFollow(ySetFollowPos);
+});
+
