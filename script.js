edf7280a5b21e20bed0985130fcec78633336644
pao-tola <73347353+pao-tola@users.noreply.github.com>
2022-03-13 16:22:38 +0700

    first commit

--- script.js
+++ script.js
@@ -1,112 +1,102 @@
-const rand = function(min, max) {
-  return Math.random() * ( max - min ) + min;
+// by
+// abubakersaeed.netlify.com | @AbubakerSaeed96
+// ============================================
+
+// Inspiration:
+// Tilt.js: https://gijsroge.github.io/tilt.js/
+// Andy Merskin's parallax depth cards pen: https://codepen.io/andymerskin/full/XNMWvQ/
+
+// Thank You for Viewing
+
+class parallaxTiltEffect {
+
+  constructor({element, tiltEffect}) {
+
+    this.element = element;
+    this.container = this.element.querySelector(".container");
+    this.size = [300, 360];
+    [this.w, this.h] = this.size;
+
+    this.tiltEffect = tiltEffect;
+
+    this.mouseOnComponent = false;
+
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    this.defaultStates = this.defaultStates.bind(this);
+    this.setProperty = this.setProperty.bind(this);
+    this.init = this.init.bind(this);
+
+    this.init();
+  }
+
+  handleMouseMove(event) {
+    const {offsetX, offsetY} = event;
+
+    let X;
+    let Y;
+
+    if (this.tiltEffect === "reverse") {
+      X = ((offsetX - (this.w/2)) / 3) / 3;
+      Y = (-(offsetY - (this.h/2)) / 3) / 3;
+    }
+
+    else if (this.tiltEffect === "normal") {
+      X = (-(offsetX - (this.w/2)) / 3) / 3;
+      Y = ((offsetY - (this.h/2)) / 3) / 3;
+    }
+
+    this.setProperty('--rY', X.toFixed(2));
+    this.setProperty('--rX', Y.toFixed(2));
+
+    this.setProperty('--bY', (80 - (X/4).toFixed(2)) + '%');
+    this.setProperty('--bX', (50 - (Y/4).toFixed(2)) + '%');
+  }
+
+  handleMouseEnter() {
+    this.mouseOnComponent = true;
+    this.container.classList.add("container--active");
+  }
+
+  handleMouseLeave() {
+    this.mouseOnComponent = false;
+    this.defaultStates();
+  }
+
+  defaultStates() {
+    this.container.classList.remove("container--active");
+    this.setProperty('--rY', 0);
+    this.setProperty('--rX', 0);
+    this.setProperty('--bY', '80%');
+    this.setProperty('--bX', '50%');
+  }
+
+  setProperty(p, v) {
+    return this.container.style.setProperty(p, v);
+  }
+
+  init() {
+    this.element.addEventListener('mousemove', this.handleMouseMove);
+    this.element.addEventListener('mouseenter', this.handleMouseEnter);
+    this.element.addEventListener('mouseleave', this.handleMouseLeave);
+  }
+
 }
 
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+const $ = e => document.querySelector(e);
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-window.addEventListener('resize', function() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  ctx = canvas.getContext('2d');
-  ctx.globalCompositeOperation = 'lighter';
+const wrap1 = new parallaxTiltEffect({
+  element: $('.wrap--1'),
+  tiltEffect: 'reverse'
 });
-let backgroundColors = [ '#000', '#000' ];
-let colors = [
-  [ '#002aff', "#009ff2" ],
-  [ '#0054ff', '#27e49b' ], 
-  [ '#202bc5' ,'#873dcc' ]
-];
-let count = 70;
-let blur = [ 12, 70 ];
-let radius = [ 1, 120 ];
 
-ctx.clearRect( 0, 0, canvas.width, canvas.height );
-ctx.globalCompositeOperation = 'lighter';
+const wrap2 = new parallaxTiltEffect({
+  element: $('.wrap--2'),
+  tiltEffect: 'normal'
+});
 
-let grd = ctx.createLinearGradient(0, canvas.height, canvas.width, 0);
-grd.addColorStop(0, backgroundColors[0]);
-grd.addColorStop(1, backgroundColors[1]);
-ctx.fillStyle = grd;
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-let items = [];
-
-while(count--) {
-    let thisRadius = rand( radius[0], radius[1] );
-    let thisBlur = rand( blur[0], blur[1] );
-    let x = rand( -100, canvas.width + 100 );
-    let y = rand( -100, canvas.height + 100 );
-    let colorIndex = Math.floor(rand(0, 299) / 100);
-    let colorOne = colors[colorIndex][0];
-    let colorTwo = colors[colorIndex][1];
-    
-    ctx.beginPath();
-    ctx.filter = `blur(${thisBlur}px)`;
-    let grd = ctx.createLinearGradient(x - thisRadius / 2, y - thisRadius / 2, x + thisRadius, y + thisRadius);
-  
-    grd.addColorStop(0, colorOne);
-    grd.addColorStop(1, colorTwo);
-    ctx.fillStyle = grd;
-    ctx.fill();
-    ctx.arc( x, y, thisRadius, 0, Math.PI * 2 );
-    ctx.closePath();
-    
-    let directionX = Math.round(rand(-99, 99) / 100);
-    let directionY = Math.round(rand(-99, 99) / 100);
-  
-    items.push({
-      x: x,
-      y: y,
-      blur: thisBlur,
-      radius: thisRadius,
-      initialXDirection: directionX,
-      initialYDirection: directionY,
-      initialBlurDirection: directionX,
-      colorOne: colorOne,
-      colorTwo: colorTwo,
-      gradient: [ x - thisRadius / 2, y - thisRadius / 2, x + thisRadius, y + thisRadius ],
-    });
-}
-
-
-function changeCanvas(timestamp) {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  let adjX = 2.251;
-  let adjY = 2.251;
-  let adjBlur = 0.6;
-  items.forEach(function(item) {
-    
-      if(item.x + (item.initialXDirection * adjX) >= canvas.width && item.initialXDirection !== 0 || item.x + (item.initialXDirection * adjX) <= 0 && item.initialXDirection !== 0) {
-        item.initialXDirection = item.initialXDirection * -1;
-      }
-      if(item.y + (item.initialYDirection * adjY) >= canvas.height && item.initialYDirection !== 0 || item.y + (item.initialYDirection * adjY) <= 0 && item.initialYDirection !== 0) {
-        item.initialYDirection = item.initialYDirection * -1;
-      }
-      
-      if(item.blur + (item.initialBlurDirection * adjBlur) >= radius[1] && item.initialBlurDirection !== 0 || item.blur + (item.initialBlurDirection * adjBlur) <= radius[0] && item.initialBlurDirection !== 0) {
-        item.initialBlurDirection *= -1;
-      }
-    
-      item.x += (item.initialXDirection * adjX);
-      item.y += (item.initialYDirection * adjY);
-      item.blur += (item.initialBlurDirection * adjBlur);
-      ctx.beginPath();
-      ctx.filter = `blur(${item.blur}px)`;
-      let grd = ctx.createLinearGradient(item.gradient[0], item.gradient[1], item.gradient[2], item.gradient[3]);
-      grd.addColorStop(0, item.colorOne);
-      grd.addColorStop(1, item.colorTwo);
-      ctx.fillStyle = grd;
-      ctx.arc( item.x, item.y, item.radius, 0, Math.PI * 2 );
-      ctx.fill();
-      ctx.closePath();
-    
-  });
-  window.requestAnimationFrame(changeCanvas);
-  
-}
-
-window.requestAnimationFrame(changeCanvas);
+const wrap3 = new parallaxTiltEffect({
+  element: $('.wrap--3'),
+  tiltEffect: 'reverse'
+});
