a8eff933d1da693a4240599a418a94e82ab90f5e
pao-tola <73347353+pao-tola@users.noreply.github.com>
2022-03-14 01:40:13 +0700

    first commit

--- script.js
+++ script.js
@@ -1,56 +1,34 @@
-var amount = 60,
-  between = (min, max) => min + Math.random() * (max - min),
-  colors = ['#275EFE', '#5C86FF', '#362A89', '#6D58FF', '#F04949', '#16BF78'],
-  current = 0;
+// gsap.set(".ball", { xPercent: -50, yPercent: -50 });
+// gsap.set(".follow-ball", { xPercent: -50, yPercent: -50 });
 
-let interval = setInterval(() => {
-  if (current < amount) {
-    animate(createConfetti());
-  }
-}, 50);
+// const ball = document.querySelector(".ball");
+// const follow = document.querySelector(".follow-ball");
+// const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+// const posF = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+// const mouse = { x: pos.x, y: pos.y };
+// const speed = 1;
+// const speedF = 0.35;
 
-setTimeout(() => clearInterval(interval), 4000);
+// const xSet = gsap.quickSetter(ball, "x", "px");
+// const ySet = gsap.quickSetter(ball, "y", "px");
+// const xSetFollow = gsap.quickSetter(follow, "x", "px");
+// const ySetFollow = gsap.quickSetter(follow, "y", "px");
 
-function createConfetti() {
-  let div = document.createElement('div');
-  gsap.set(div, {
-    attr: {
-      class: 'confetti',
-      style: '--color: ' + colors[Math.floor(Math.random() * colors.length)]
-    },
-    x: between(0, window.innerWidth),
-    y: -window.innerHeight / 4,
-    z: between(-80, 80)
-  });
-  current++;
-  document.body.appendChild(div);
-  return div;
-}
+// window.addEventListener("mousemove", e => {
+//   mouse.x = e.x;
+//   mouse.y = e.y;
+// });
 
-function animate(element) {
-  gsap.to(element, {
-    y: window.innerHeight + 40,
-    ease: 'power1.out',
-    delay: between(0, .25),
-    duration: between(2, 5),
-    onComplete() {
-      if (element instanceof Element || element instanceof HTMLDocument) {
-        current--;
-        element.remove();
-      }
-    }
-  });
-  gsap.to(element, {
-    rotationZ: between(90, 180),
-    repeat: -1,
-    yoyo: true,
-    duration: between(3, 6)
-  });
-  gsap.to(element, {
-    rotationX: between(0, 360),
-    rotationY: between(0, 360),
-    repeat: -1,
-    yoyo: true,
-    duration: between(3, 6)
-  });
-}
+// gsap.ticker.add(() => {
+//   const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+//   const dtF = 1.0 - Math.pow(1.0 - speedF, gsap.ticker.deltaRatio());
+//   const xSetPos = pos.x += (mouse.x - pos.x) * dt;
+//   const ySetPos = pos.y += (mouse.y - pos.y) * dt;
+//   const xSetFollowPos = posF.x += (mouse.x - posF.x) * dtF;
+//   const ySetFollowPos = posF.y += (mouse.y - posF.y) * dtF;
+
+//   xSet(xSetPos);
+//   ySet(ySetPos);
+//   xSetFollow(xSetFollowPos);
+//   ySetFollow(ySetFollowPos);
+// });
