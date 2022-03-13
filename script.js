9eea021e0ec32a7459912f6ae836d3e2541201e2
pao-tola <73347353+pao-tola@users.noreply.github.com>
2022-03-13 18:49:22 +0700

    first commit

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
-gsap.set(".ball", { xPercent: -50, yPercent: -50 });
-gsap.set(".follow-ball", { xPercent: -50, yPercent: -50 });
+// gsap.set(".ball", { xPercent: -50, yPercent: -50 });
+// gsap.set(".follow-ball", { xPercent: -50, yPercent: -50 });
 
-const ball = document.querySelector(".ball");
-const follow = document.querySelector(".follow-ball");
-const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-const posF = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-const mouse = { x: pos.x, y: pos.y };
-const speed = 1;
-const speedF = 0.35;
+// const ball = document.querySelector(".ball");
+// const follow = document.querySelector(".follow-ball");
+// const pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+// const posF = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
+// const mouse = { x: pos.x, y: pos.y };
+// const speed = 1;
+// const speedF = 0.35;
 
-const xSet = gsap.quickSetter(ball, "x", "px");
-const ySet = gsap.quickSetter(ball, "y", "px");
-const xSetFollow = gsap.quickSetter(follow, "x", "px");
-const ySetFollow = gsap.quickSetter(follow, "y", "px");
+// const xSet = gsap.quickSetter(ball, "x", "px");
+// const ySet = gsap.quickSetter(ball, "y", "px");
+// const xSetFollow = gsap.quickSetter(follow, "x", "px");
+// const ySetFollow = gsap.quickSetter(follow, "y", "px");
 
-window.addEventListener("mousemove", e => {
-  mouse.x = e.x;
-  mouse.y = e.y;
-});
+// window.addEventListener("mousemove", e => {
+//   mouse.x = e.x;
+//   mouse.y = e.y;
+// });
 
-gsap.ticker.add(() => {
-  const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
-  const dtF = 1.0 - Math.pow(1.0 - speedF, gsap.ticker.deltaRatio());
-  const xSetPos = pos.x += (mouse.x - pos.x) * dt;
-  const ySetPos = pos.y += (mouse.y - pos.y) * dt;
-  const xSetFollowPos = posF.x += (mouse.x - posF.x) * dtF;
-  const ySetFollowPos = posF.y += (mouse.y - posF.y) * dtF;
+// gsap.ticker.add(() => {
+//   const dt = 1.0 - Math.pow(1.0 - speed, gsap.ticker.deltaRatio());
+//   const dtF = 1.0 - Math.pow(1.0 - speedF, gsap.ticker.deltaRatio());
+//   const xSetPos = pos.x += (mouse.x - pos.x) * dt;
+//   const ySetPos = pos.y += (mouse.y - pos.y) * dt;
+//   const xSetFollowPos = posF.x += (mouse.x - posF.x) * dtF;
+//   const ySetFollowPos = posF.y += (mouse.y - posF.y) * dtF;
 
-  xSet(xSetPos);
-  ySet(ySetPos);
-  xSetFollow(xSetFollowPos);
-  ySetFollow(ySetFollowPos);
-});
+//   xSet(xSetPos);
+//   ySet(ySetPos);
+//   xSetFollow(xSetFollowPos);
+//   ySetFollow(ySetFollowPos);
+// });
